7f40baafe5d15f5204a330ed18416ef881668fd1
Gorzo <galaxycoff@gmail.com>
2026-02-09 16:31:08 -0500

    wham stuff

--- wham.R
+++ wham.R
@@ -1,2 +1,3 @@
 library(wham)
 asap <- read_asap3_dat("C:/Users/jgorzo/OneDrive - New Jersey Office of Information Technology/Documents/output/tog/asap/FINAL/ORIG.DAT")
+input_asap <- prepare_wham_input(asap)
