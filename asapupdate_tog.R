ff76204a3970e0b5a0a6b7441dc5944f33a4f9f4
Gorzo <galaxycoff@gmail.com>
2026-02-20 16:26:45 -0500

    auto updating wham

--- asapupdate_tog.R
+++ asapupdate_tog.R
@@ -1,5 +1,7 @@
 library(wham)
-n <- 4 #number of new years
+endyr <- 2024
 asap <- read_asap3_dat("C:/Users/jgorzo/OneDrive - New Jersey Office of Information Technology/Documents/output/tog/asap/FINAL/ORIG.DAT")
+n <- endyr - asap[[1]]$dat$R_avg_end
+asap[[1]]$dat$R_avg_end <- endyr
 asap[[1]]$dat$n_years <- asap[[1]]$dat$n_years + n
 asap[[1]]$dat$M <- rbind(asap[[1]]$dat$M, do.call("rbind", replicate(n, asap[[1]]$dat$M[1,], simplify = FALSE)))
