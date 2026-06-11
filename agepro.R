4f77e4757671adf373d56e5d92daa62465163c8e
Gorzo <galaxycoff@gmail.com>
2026-06-11 15:06:22 -0400

    agepro R

--- agepro.R
+++ agepro.R
@@ -6,12 +6,22 @@ bsnfile <- file.path("C:/Users/jgorzo/OneDrive - New Jersey Office of Informatio
 
 # Create a agepro_inp_model with default values (This will 
 test <- ageproR::agepro_inp_model$new()
+test$general$yr_start <- 2025
+test$general$yr_end <- 2125
 test$read_inp(inpfile)
+test$general <- general_params$new(yr_start = 2025,
+                                   yr_end = 2125,
+                                   age_begin = test$general$age_begin,
+                                   age_end = test$general$age_end,
+                                   num_pop_sims = test$general$num_pop_sims,
+                                   num_fleets = test$general$num_fleets,
+                                   num_rec_models = test$general$num_rec_models,
+                                   discards_present = test$general$discards_present,
+                                   seed = test$general$seed)
 # NOTE: This will give a WARNING that a NULL Recruitment Model is Found. This behavior is expected. 
 # ageproR will check to see if agepro models will have NULL or invalid Recruitment models before being saved to file
 test$set_bootstrap_filename(bsnfile)
-test$general$yr_start <- 2025
-test$general$yr_end <- 2125
+
 recr <- read.csv("C:/Users/jgorzo/OneDrive - New Jersey Office of Information Technology/Documents/output/tog/asap/vtsage/plots/ASAP_summary_vtsage.csv")
 test$recruit$recruit_data[[1]]$observations <- as.matrix(recr$Recr)
 colnames(test$recruit$recruit_data[[1]]$observations) <- "recruit"
